a50b50c5538da67e4c88b5d1601a0ac661e3322f
Alayna Li <alaynali.123@gmail.com>
2024-12-05 17:37:16 -0600

    draw lasers using equations

--- laser_harp_test/Images/lasers/lasers/lasers_palette.sv
+++ laser_harp_test/Images/lasers/lasers/lasers_palette.sv
@@ -10,12 +10,12 @@ localparam [0:31][11:0] palette = {
 	{4'h3, 4'h3, 4'h3},
 	{4'h5, 4'h5, 4'h5},
 	{4'hF, 4'h3, 4'h3}, // red
-	{4'h0, 4'h4, 4'hA},
+	{4'h0, 4'h4, 4'hA}, // indigo
 	{4'hF, 4'hD, 4'h5}, // yellow
 	{4'hA, 4'hA, 4'hF},
-	{4'h5, 4'h1, 4'hE},
+	{4'h5, 4'h1, 4'hE}, // violet
 	{4'hF, 4'hF, 4'hF},
-	{4'h3, 4'hB, 4'hF},
+	{4'h3, 4'hB, 4'hF}, // light blue
 	{4'hD, 4'h2, 4'hD},
 	{4'h1, 4'h1, 4'h1},
 	{4'h4, 4'h5, 4'h5},
