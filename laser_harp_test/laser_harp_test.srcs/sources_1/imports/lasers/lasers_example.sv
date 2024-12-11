2ccdf281f298780555e9f7c7d7c0eeb31df22b20
Alayna Li <alaynali.123@gmail.com>
2024-12-11 16:27:43 -0600

    add right click functionality to all other lasers

--- laser_harp_test/laser_harp_test.srcs/sources_1/imports/lasers/lasers_example.sv
+++ laser_harp_test/laser_harp_test.srcs/sources_1/imports/lasers/lasers_example.sv
@@ -268,8 +268,8 @@ end
 
 always_comb
 begin:Orange_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 196 && CursorX <= 206) begin // if (CursorY <= 350 && CursorY >= 341 && CursorX >= 194 && CursorX <= 204 && orange_click) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 196 && CursorX <= 206 || (r_click && CursorY >= 3*CursorX-264 && CursorY <= 3*CursorX-234 && CursorY <= 355 && CursorY >= 11) begin // if (CursorY <= 350 && CursorY >= 341 && CursorX >= 194 && CursorX <= 204 && orange_click) begin
 			orange_click_next = 1'b0;
 			OrangeY_next = 9'd10;
 		end
@@ -291,8 +291,8 @@ end
 
 always_comb
 begin:Yellow_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 217 && CursorX <= 227) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 217 && CursorX <= 227 || (r_click && CursorY >= 6*CursorX-1008 && CursorY <= 6*CursorX-948 && CursorY <= 355 && CursorY >= 11)) begin
 			yellow_click_next = 1'b0;
 			YellowY_next = 9'd10;
 		end
@@ -314,8 +314,8 @@ end
 
 always_comb
 begin:Green_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 238 && CursorX <= 248) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 238 && CursorX <= 248 || (r_click && CursorX >= 238 && CursorX <= 248 && CursorY <= 355 && CursorY >= 11)) begin
 			green_click_next = 1'b0;
 			GreenY_next = 9'd10;
 		end
@@ -335,8 +335,8 @@ begin:Green_int_proc
 end
 always_comb
 begin:Blue_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 259 && CursorX <= 269) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 259 && CursorX <= 269 || (r_click && CursorY >= -6*CursorX+1908 && CursorY <= -6*CursorX+1968 && CursorY <= 355 && CursorY >= 11)) begin
 			blue_click_next = 1'b0;
 			BlueY_next = 9'd10;
 		end
@@ -356,8 +356,8 @@ begin:Blue_int_proc
 end
 always_comb
 begin:Indigo_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 280 && CursorX <= 290) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 280 && CursorX <= 290 || (r_click && CursorY >= -3*CursorX+1194 && CursorY <= -3*CursorX+1224 && CursorY <= 355 && CursorY >= 11)) begin
 			indigo_click_next = 1'b0;
 			IndigoY_next = 9'd10;
 		end
@@ -377,8 +377,8 @@ begin:Indigo_int_proc
 end
 always_comb
 begin:Violet_int_proc
-	if (l_click) begin
-	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 302 && CursorX <= 312) begin
+	if (l_click || r_click) begin
+	   if (CursorY >= 347 && CursorY <= 357 && CursorX >= 302 && CursorX <= 312 || (r_click && CursorY >= -2*CursorX+956 && CursorY <= -2*CursorX+976 && CursorY <= 355 && CursorY >= 11)) begin
 			violet_click_next = 1'b0;
 			VioletY_next = 9'd10;
 		end
